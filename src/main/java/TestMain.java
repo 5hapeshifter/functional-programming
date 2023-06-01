9f91eecb1581e88470390c8b72ad59dc028edab1
5hapeshifter <suleimandiverio@gmail.com>
2023-06-01 18:35:51 -0300

    Single Line vs Multi Line Lambda

--- src/main/java/TestMain.java
+++ src/main/java/TestMain.java
@@ -9,17 +9,23 @@ public class TestMain {
         Dog d = new Dog();
 
         // implementacao da interface com classe anonima
-        GreetingService hiService = new GreetingService() {
-            @Override
-            public void greet(String name) {
-                System.out.println("Hi..." + name);
-            }
-        };
-        hiService.greet("Suleiman");
+//        GreetingService hiService = new GreetingService() {
+//            @Override
+//            public void greet(String name) {
+//                System.out.println("Hi..." + name);
+//            }
+//        };
+//        hiService.greet("Suleiman");
 
-        // implementacao da interface com lambda, A INTERFACE SO MODE TER UM METODO ABSTRATO
-        GreetingService helloService = (name) -> System.out.println("Hello..." + name);
-        helloService.greet("Suleiman");
+        // implementacao da interface com lambda em single line declaration, somente com uma execucao
+        //GreetingService helloService = (name) -> System.out.println("Hello..." + name);
+
+        // implementacao com mais de uma linha, para executar mais de uma funcao
+        GreetingService helloService = (fn, ln, age) -> {
+            System.out.println("my name :: " + fn + " " + ln);
+            System.out.println("my age :: " + age);
+        };
+        helloService.greet("Suleiman", "Diverio", 35);
 
 
     }
--- src/main/java/lambda/GreetingService.java
+++ src/main/java/lambda/GreetingService.java
@@ -1,6 +1,9 @@
 package lambda;
 
+// SAM - single abstract method - uma interface abstrata sempre tera SOMENTE um metodo abstrato
+@FunctionalInterface
 public interface GreetingService {
 
-    void greet(String name);
+    void greet(String firstName, String lastName, int age);
+
 }
