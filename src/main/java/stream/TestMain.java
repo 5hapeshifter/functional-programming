1682bd54e554f63529efa3ddc7c4ce39f15b0f20
5hapeshifter <suleimandiverio@gmail.com>
2023-06-15 20:27:40 -0300

    97. Google Links Assignment Improvements - Demo

--- src/main/java/stream/TestMain.java
+++ src/main/java/stream/TestMain.java
@@ -10,8 +10,8 @@ public class TestMain {
     public static void main(String[] args) {
 
         List<Integer> list = new ArrayList<>();
-        list.add(8);
-        list.add(9);
+//        list.add(8);
+//        list.add(9);
         list.add(1);
         list.add(2);
         list.add(3);
@@ -19,8 +19,8 @@ public class TestMain {
         list.add(5);
         list.add(6);
         list.add(7);
-//        list.add(8); //
-//        list.add(9);
+        list.add(8);
+        list.add(9);
 
 
 //        list.stream()
@@ -33,23 +33,96 @@ public class TestMain {
 //                .forEach(System.out::println);
 
         // Fluxo de execucao da stream
+        /*
+            Testando sorted()
+         */
+//        System.out.println("starting");
+//
+//        Predicate<Integer> evenNumber = (i) -> i % 2 == 0;
+//
+//        list.stream()
+//                .filter(i -> {
+//                    System.out.println("filter-received :: " + i);
+//                    return evenNumber.test(i);
+//                        })
+//                //.sorted() // quando usamos o sorted(), todos os dados da lista sao recebidos e ordenados, depois sao repassados a proxima execucao
+//                .limit(3) // Limita os objetos avaliados
+//                .sorted()
+//                .map(i -> {
+//                    // map eh o unico que pode mudar o valor na pipeline de stream
+//                    System.out.println("map-received :: " + i);
+//                    return i * i;
+//                })
+//                .forEach(System.out::println);
+//
+//        System.out.println("ending");
+
+
+        /*
+            Testando peek
+         */
+//        System.out.println("starting");
+//
+//        Predicate<Integer> evenNumber = (i) -> i % 2 == 0;
+//
+//        list.stream()
+//                // peek e utilizado para debugar
+//                .peek(i -> System.out.println("filter-received :: " + i))
+//                .filter(i -> {
+//                    //System.out.println("filter-received :: " + i);
+//                    return evenNumber.test(i);
+//                })
+//                .limit(3)
+//                .peek(i -> System.out.println("map-received :: " + i))
+//                .map(i ->  i * i)
+//                .forEach(System.out::println);
+//
+//        System.out.println("ending");
+
+        /*
+            Testando skip
+         */
+//        System.out.println("starting");
+//
+//        Predicate<Integer> evenNumber = (i) -> i % 2 == 0;
+//
+//        list.stream()
+//                // a quantidade de objetos informada no skip sera ignorada
+//                //.skip(4)
+//                .peek(i -> System.out.println("filter-received :: " + i))
+//                .filter(i -> {
+//                    //System.out.println("filter-received :: " + i);
+//                    return evenNumber.test(i);
+//                })
+//                .limit(3) // 2,4,6
+//                .peek(i -> System.out.println("map-received :: " + i))
+//                .map(i ->  i * i)
+//                .skip(2) // pulou o 2 e 4
+//                .forEach(System.out::println);
+//
+//        System.out.println("ending");
+
+
+        /*
+            Mais sobre filter e map
+         */
         System.out.println("starting");
 
-        Predicate<Integer> evenNumber = (i) -> i % 2 == 0;
+        // verificacao mais complexa
+        Predicate<Integer> evenNumber = (i) -> i % 2 == 0 && i > 5;
 
         list.stream()
+                // a quantidade de objetos informada no skip sera ignorada
+                //.skip(4)
+                .peek(i -> System.out.println("filter-received :: " + i))
                 .filter(i -> {
-                    System.out.println("filter-received :: " + i);
+                    //System.out.println("filter-received :: " + i);
                     return evenNumber.test(i);
-                        })
-                //.sorted() // quando usamos o sorted(), todos os dados da lista sao recebidos e ordenados, depois sao repassados a proxima execucao
-                .limit(3)
-                .sorted()
-                .map(i -> {
-                    // map eh o unico que pode mudar o valor na pipeline de stream
-                    System.out.println("map-received :: " + i);
-                    return i * i;
                 })
+                //.filter(i -> i > 5) // Podemos adicionar mais de um filter ou map
+                .peek(i -> System.out.println("map-received :: " + i))
+                .map(i ->  i * i)
+                //.map(i -> i + 2) // Podemos adicionar mais de um filter ou map
                 .forEach(System.out::println);
 
         System.out.println("ending");
--- src/test/java/test/DriverTest.java
+++ src/test/java/test/DriverTest.java
@@ -28,25 +28,58 @@ public class DriverTest {
 //                .forEach(System.out::println);
 //    }
 
+//    @Test
+//    public void googleTest() {
+//        this.driver.get("https://google.com");
+//        List<WebElement> elements = this.driver.findElements(By.tagName("a"));
+////        Predicate<WebElement> isBlank = (e) -> e.getText().trim().length() == 0;
+////        Predicate<WebElement> hasS = (e) -> e.getText().toLowerCase().contains("s");
+//
+//        System.out.println("Before :: " + elements.size());
+//        //elements.removeIf(isBlank);
+//
+//        //Aplicando uma lista de regras dos Predicates a uma lista de elementos.
+////        Rules.get().forEach(rule -> elements.removeIf(rule));
+//        Rules.get().forEach(elements::removeIf);
+//
+//        //chamada encadeada
+////        elements.removeIf(isBlank.or(hasS));
+//
+//        System.out.println("After :: " + elements.size());
+//        elements.forEach(e -> System.out.println(e.getText()));
+//    }
+
+    // do not allow blank links
+    // do not allow links containing s
+    // convert link text to uppercase
+    // print on the console
+//    @Test
+//    public void googleTest() {
+//        this.driver.get("https://google.com");
+//        this.driver.findElements(By.tagName("a"))
+//                .stream()
+//                .filter(e -> e.getText().trim().length() > 0)
+//                .filter(e -> !e.getText().toLowerCase().contains("s"))
+//                .map(e -> e.getText().toUpperCase())
+//                .forEach(System.out::println);
+//
+//    }
+
+    /*
+        Outra forma de fazer o metodo acima usando method reference
+     */
     @Test
     public void googleTest() {
         this.driver.get("https://google.com");
-        List<WebElement> elements = this.driver.findElements(By.tagName("a"));
-//        Predicate<WebElement> isBlank = (e) -> e.getText().trim().length() == 0;
-//        Predicate<WebElement> hasS = (e) -> e.getText().toLowerCase().contains("s");
+        this.driver.findElements(By.tagName("a"))
+                .stream()
+                .map(WebElement::getText)
+                .map(String::trim)
+                .filter(e -> e.length() > 0)
+                .filter(e -> !e.toLowerCase().contains("s"))
+                .map(String::toUpperCase)
+                .forEach(System.out::println);
 
-        System.out.println("Before :: " + elements.size());
-        //elements.removeIf(isBlank);
-
-        //Aplicando uma lista de regras dos Predicates a uma lista de elementos.
-//        Rules.get().forEach(rule -> elements.removeIf(rule));
-        Rules.get().forEach(elements::removeIf);
-
-        //chamada encadeada
-//        elements.removeIf(isBlank.or(hasS));
-
-        System.out.println("After :: " + elements.size());
-        elements.forEach(e -> System.out.println(e.getText()));
     }
 
     @AfterTest
