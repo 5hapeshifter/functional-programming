a2ca2ca9b0a3c4f8c32d6ff0efec6c311c0d4933
5hapeshifter <suleimandiverio@gmail.com>
2023-06-15 15:48:09 -0300

    82. Finding Squares Of Even Number Using Stream

---
+++ src/main/java/stream/TestMain.java
@@ -0,0 +1,32 @@
+package stream;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestMain {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        list.add(9);
+
+        list.stream()
+                /*
+                    Numeros encontrados / numero * numero
+                 */
+                .filter(integer -> integer % 2 == 0)
+                .limit(3)
+                .map(integer -> integer * integer)
+                .forEach(System.out::println);
+        ;
+
+    }
+}
